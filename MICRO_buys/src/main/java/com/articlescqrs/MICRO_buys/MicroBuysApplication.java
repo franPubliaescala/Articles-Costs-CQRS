9faecd1707b1e12267fb322b6cebd01befb4c860
Fran <fran@publiaescala.com>
2022-10-23 18:23:29 +0200

    Añadido el proyecto base del microservicio de compras

---
+++ MICRO_buys/src/main/java/com/articlescqrs/MICRO_buys/MicroBuysApplication.java
@@ -0,0 +1,13 @@
+package com.articlescqrs.MICRO_buys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBuysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBuysApplication.class, args);
+	}
+
+}
---
+++ MICRO_buys/src/test/java/com/articlescqrs/MICRO_buys/MicroBuysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articlescqrs.MICRO_buys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBuysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
